19828b8f361160af53aeece2a4b2bbf653095ae0
xfreshu <lprzybyszcontact@gmail.com>
2021-03-30 19:43:33 +0200

    added axios with datoCMS

--- src/components/organisms/NewsSection/NewsSection.js
+++ src/components/organisms/NewsSection/NewsSection.js
@@ -1,47 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NewsSectionHeader, Wrapper } from './NewSection.styled';
 import { ArticleWrapper, TitleWrapper, ContentWrapper } from './NewSection.styled';
 import { Button } from '../../atoms/Button/Button';
+import axios from 'axios';
 
-const data = [
-  {
-    title: 'Test title1',
-    category: 'Test category',
-    content: 'Test content',
-    image: null,
-  },
-  {
-    title: 'Test title2',
-    category: 'Test category',
-    content:
-      'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem ',
-    image: 'https://unsplash.it/500/400',
-  },
-  {
-    title: 'Test title3',
-    category: 'Test category',
-    content: 'Test content',
-    image: null,
-  },
-];
+const API_TOKEN = 'cda86ed94bec23980cc416a58f903c';
 
 const NewsSection = () => {
+  const [articles, setArticles] = useState([]);
+  axios
+    .post(
+      'https://graphql.datocms.com/',
+      {
+        query: `
+      {
+        allArticles{
+          id
+          title
+          category
+          content
+          image{
+            url
+          }
+        }
+      }`,
+      },
+      {
+        headers: {
+          authorization: `Bearer ${API_TOKEN}`,
+        },
+      }
+    )
+    .then(({ data: { data } }) => {
+      setArticles(data.allArticles);
+    })
+    .catch((err) => console.log(err));
   return (
     <Wrapper>
       <NewsSectionHeader>News feed section</NewsSectionHeader>
-      {data.map(({ title, content, image = null, category }) => (
-        <ArticleWrapper key={title}>
-          <TitleWrapper>
-            <h3>{title}</h3>
-            <p>{category}</p>
-          </TitleWrapper>
-          <ContentWrapper>
-            <p>{content}</p>
-            {image ? <img src={image} alt="article" /> : null}
-          </ContentWrapper>
-          <Button isBig>click me</Button>
-        </ArticleWrapper>
-      ))}
+      {articles.length > 0 ? (
+        articles.map(({ title, content, image = null, category }) => (
+          <ArticleWrapper key={title}>
+            <TitleWrapper>
+              <h3>{title}</h3>
+              <p>{category}</p>
+            </TitleWrapper>
+            <ContentWrapper>
+              <p>{content}</p>
+              {image ? <img src={image.url} alt="article" /> : null}
+            </ContentWrapper>
+            <Button isBig>click me</Button>
+          </ArticleWrapper>
+        ))
+      ) : (
+        <TitleWrapper>Loading</TitleWrapper>
+      )}
     </Wrapper>
   );
 };
